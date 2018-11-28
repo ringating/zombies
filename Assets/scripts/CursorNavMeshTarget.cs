f78e55cdea88469f3ab3f88cfdc176a676690591
ringating <34548930+ringating@users.noreply.github.com>
2018-11-28 12:49:42 -0800

    a lot
    added a navmesh to the test scene
    added a test zombie
    added a script to set a navmesh agent destination to the cursor's position
    
    other things

---
+++ Assets/scripts/CursorNavMeshTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CursorNavMeshTarget : MonoBehaviour 
+{
+	public NavMeshAgent nmAgent;
+	public Camera cam;
+	
+	void Update () 
+	{
+		this.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+		nmAgent.destination = this.transform.position;
+	}
+}
---
+++ Assets/scripts/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tools
+{
+	public static Vector3 V2toV3(Vector2 v2)
+	{
+		return new Vector3(v2.x, 0, v2.y);
+	}
+
+	public static Vector3 V2toV3(Vector2 v2, float y)
+	{
+		return new Vector3(v2.x, y, v2.y);
+	}
+
+	public static Vector2 V3toV2(Vector3 v3)
+	{
+		return new Vector2(v3.x, v3.z);
+	}
+}
