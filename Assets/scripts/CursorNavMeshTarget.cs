fe74dcd97110251b6ca99a7bde4871affd599536
ringating <34548930+ringating@users.noreply.github.com>
2018-11-29 11:29:39 -0800

    added test hoard, updated Zombie.cs

--- Assets/scripts/CursorNavMeshTarget.cs
+++ Assets/scripts/CursorNavMeshTarget.cs
@@ -5,12 +5,17 @@ using UnityEngine.AI;
 
 public class CursorNavMeshTarget : MonoBehaviour 
 {
-	public NavMeshAgent nmAgent;
+	public NavMeshAgent[] nmAgents;
 	public Camera cam;
 	
 	void Update () 
 	{
 		this.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
-		nmAgent.destination = this.transform.position;
+		//nmAgent.destination = this.transform.position;
+
+		foreach (NavMeshAgent nma in nmAgents)
+		{
+			nma.destination = this.transform.position;
+		}
 	}
 }
--- Assets/scripts/Zombie.cs
+++ Assets/scripts/Zombie.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
 	public float hitTimer;
 	public float timeToHit;
+	[HideInInspector]
+	public NavMeshAgent nma;
+
+	void Start()
+	{
+		nma = GetComponent<NavMeshAgent>();
+	}
 }
