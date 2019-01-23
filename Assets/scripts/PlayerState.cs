e9e833714328c34d31756651d1fbc6d684e32c57
ringating <34548930+ringating@users.noreply.github.com>
2019-01-23 11:47:08 -0800

    updated to use rigidbody stuff for player movement
    for the sake of making resolving collision with zombies easier

--- Assets/scripts/PlayerState.cs
+++ Assets/scripts/PlayerState.cs
@@ -15,7 +15,7 @@ public class PlayerState : MonoBehaviour
 	public float acceleration; // in % of walkSpeed per sec
 
     public AxisHandler axes;
-    public Rigidbody rigid;
+    public Rigidbody rb;
     public NavMeshAgent nma;
 
 	// variables
@@ -71,7 +71,8 @@ public class PlayerState : MonoBehaviour
             vel.Set(vel.x, vel.y+acceleration);
             if(vel.y > targetVel.y){ vel.Set(vel.x, targetVel.y); }
         }
-        //rigid.position += new Vector3(vel.x * Time.deltaTime, 0, vel.y * Time.deltaTime);
-        nma.velocity = new Vector3(vel.x, 0, vel.y);
+        //rb.position += new Vector3(vel.x * Time.deltaTime, 0, vel.y * Time.deltaTime);
+        //nma.velocity = new Vector3(vel.x, 0, vel.y);
+        rb.velocity = new Vector3(vel.x, 0, vel.y);
 	}
 }
