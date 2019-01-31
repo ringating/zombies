96ed495e244f54a3247b551f88cc0843bde796f6
ringating <34548930+ringating@users.noreply.github.com>
2019-01-30 17:06:31 -0800

    added FpsText, changed font, disabled vsync

---
+++ Assets/scripts/FpsText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsText : MonoBehaviour 
+{
+	public Text textComponent;
+
+	void Start () 
+	{
+		textComponent.text = "0";
+	}
+	
+	void Update () 
+	{
+		textComponent.text = (1/Time.deltaTime).ToString().Split('.')[0];
+	}
+}
