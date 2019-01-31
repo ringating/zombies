ed492e2b2851ca3055e3f4c45c406654c5533b55
ringating <34548930+ringating@users.noreply.github.com>
2019-01-31 10:18:06 -0800

    updated text announcements

---
+++ Assets/scripts/HUD/Announcements.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TextAnnouncement
+{
+	public string name;
+	public Text textScript;
+	public float timer;
+	public float fadeTimer;
+	public float fadeDuration;
+
+	private Color defaultColor = new Color(1,1,1,1);
+
+	public TextAnnouncement(string name, Text text)
+	{
+		this.init(name, text, 0f, 0f, defaultColor);
+	}
+
+	private void init(string name, Text textScript, float timer, float fadeTimer, Color color)
+	{
+		this.name = name;
+		this.textScript = textScript;
+		this.timer = timer;
+		this.fadeTimer = fadeTimer;
+		this.fadeDuration = fadeTimer;
+		this.textScript.color = color;
+
+		this.textScript.text = "";
+	}
+
+	public TextAnnouncement setText(string text)
+	{
+		this.textScript.text = text;
+		return this;
+	}
+
+	public TextAnnouncement setTimer(float duration)
+	{
+		this.timer = duration;
+		return this;
+	}
+
+	public TextAnnouncement setFadeTimer(float fadeDuration)
+	{
+		this.fadeTimer = fadeDuration;
+		this.fadeDuration = fadeDuration;
+		return this;
+	}
+
+	public TextAnnouncement setAlpha(float alpha)
+	{
+		this.textScript.color = new Color(this.textScript.color.r, this.textScript.color.g, this.textScript.color.b, alpha);
+		return this;
+	}
+
+	public TextAnnouncement setColor(Color color)
+	{
+		this.textScript.color = color;
+		return this;
+	}
+}
+
+public class Announcements : MonoBehaviour 
+{
+	public Text[] textAnnouncementLocations;
+	public string[] textAnnouncementLocationNames;
+
+	private List<TextAnnouncement> ta = new List<TextAnnouncement>();
+	
+	void Start() 
+	{
+		for(int i = 0; i < textAnnouncementLocations.Length; ++i)
+		{
+			ta.Add(new TextAnnouncement(textAnnouncementLocationNames[i], textAnnouncementLocations[i]));
+		}
+	}
+
+	void Update()
+	{
+		for(int i = 0; i < ta.Count; ++i)
+		{
+			// everything to do with durations
+			if(ta[i].timer > 0)
+			{
+				ta[i].timer -= Time.deltaTime;
+				ta[i].setAlpha(1);
+				if(ta[i].timer < 0)
+				{ 
+					ta[i].timer = 0; 
+					if(ta[i].fadeTimer <= 0){ ta[i].setAlpha(0); }
+				}
+			}
+			else if(ta[i].fadeTimer > 0)
+			{
+				ta[i].fadeTimer -= Time.deltaTime;
+				ta[i].setAlpha(Mathf.Lerp(0, 1, ta[i].fadeTimer/ta[i].fadeDuration));
+				if(ta[i].fadeTimer < 0){ ta[i].fadeTimer = 0; }
+			}
+		}
+	}
+	
+	public void Announce(string message, int announcementIndex, float duration, float fadeDuration)
+	{
+		ta[announcementIndex].setText(message).setTimer(duration).setFadeTimer(fadeDuration);
+	}
+
+	public void Announce(string message, string announcementLocationName, float duration, float fadeDuration)
+	{
+		Announce(message, GetAnnLocIndexByName(announcementLocationName), duration, fadeDuration);
+	}
+
+	private int GetAnnLocIndexByName(string name)
+	{
+		int index = 0;
+		for(; index < ta.Count; ++index)
+		{
+			if(ta[index].name == name)
+			{
+				break;
+			}
+		}
+		if(index >= ta.Count){index = -1;}
+		return index;
+	}
+}
---
+++ Assets/scripts/HUD/TestAnnouncement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnnouncement : MonoBehaviour 
+{	
+	public Announcements announcementManager;
+
+	public string announcementName;
+	public string message;
+	public float duration;
+	public float fadeDuration;
+
+
+	public bool testAnnouncement;
+
+	
+	void Update () 
+	{
+		if(testAnnouncement)
+		{
+			testAnnouncement = false;
+			announcementManager.Announce(message, announcementName, duration, fadeDuration);
+		}
+	}
+}
