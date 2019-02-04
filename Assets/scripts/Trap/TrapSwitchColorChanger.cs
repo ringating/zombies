0393e633f418cb3486823b6d20383cb266584412
ringating <34548930+ringating@users.noreply.github.com>
2019-02-04 11:48:53 -0800

    added TrapSwitchColorChanger

---
+++ Assets/scripts/Trap/TrapSwitchColorChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSwitchColorChanger : MonoBehaviour 
+{
+	public MeshRenderer trapMR;
+	public TrapSwitch trapSwitchScript;
+	public Color noneReadyColor = Color.red;
+	public Color someReadyColor = new Color(1f, 0.5f, 0f, 1); // orange
+	public Color allReadyColor = Color.green;
+	
+	void Update()
+	{
+		switch(trapSwitchScript.state)
+		{
+			case TrapSwitchState.noneReady:
+				trapMR.material.color = noneReadyColor;
+				break;
+			case TrapSwitchState.someReady:
+				trapMR.material.color = someReadyColor;
+				break;
+			case TrapSwitchState.allReady:
+				trapMR.material.color = allReadyColor;
+				break;
+		}
+	}
+}
